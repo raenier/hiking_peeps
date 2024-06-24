f946055ddcde93dfbf0de545273f0aabe0424de3
ren <raenierquiambao@gmail.com>
2024-06-24 12:46:11 +0800

    Show individual post page and add styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,8 @@ class PostsController < ApplicationController
   def index
     @posts = Post.includes(:user).where(user_id: current_user.following.ids << current_user.id)
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
 end
