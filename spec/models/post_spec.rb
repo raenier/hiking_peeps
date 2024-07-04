1328929caa84b4dbf0fec516ee49ef45cf22f611
ren <raenierquiambao@gmail.com>
2024-07-04 15:24:37 +0800

    Test Post associations, belongs to user, have many comments and likes

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to have_many(:comments) }
+    it { is_expected.to have_many(:likes) }
+  end
+end
