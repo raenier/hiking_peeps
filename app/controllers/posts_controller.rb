e27dfb1998ec5814453be06ea083dfa54b9550ec
ren <raenierquiambao@gmail.com>
2024-07-04 14:26:24 +0800

    Add modal for creating Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,27 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.includes(:user).where(user_id: current_user.following.ids << current_user.id)
+    @post = Post.new
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
