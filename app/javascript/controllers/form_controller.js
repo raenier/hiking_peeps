49d04c001678064e9542a5a99f48bc15e6da5639
ren <raenierquiambao@gmail.com>
2024-08-30 16:29:52 +0800

    Prevent uploading of file greater than 10MB

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -8,4 +8,11 @@ export default class extends Controller {
     this.element.querySelector(".errors").style.display = "none"
     this.element.querySelectorAll(".field_with_errors").forEach(field => field.classList.remove("field_with_errors"))
   }
+
+  checkSize(event) {
+    if (event.target.files[0].size > 10485760) {
+      event.target.value = ""
+      alert("File is too big! Upload only 10MB or less.");
+    }
+  }
 }
