a0bc1ab86bf11ee34585be6ff0c79e642eefc466
ren <raenierquiambao@gmail.com>
2024-08-30 15:38:39 +0800

    Allow users to upload multiple pictures for their posts
    To use active storage we've run the following commands
    rails active_storage:install
    rails db:migrate

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
 
   has_many :comments, as: :commentable
   has_many :likes, as: :likeable
+  has_many_attached :images
 
   validates :title, :body, presence: true
 end
