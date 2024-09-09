ef10e69fca31cb291679878d66e1c25d01afabd5
ren <raenierquiambao@gmail.com>
2024-09-09 12:49:01 +0800

    Style profile banner
    Allow immediate upload of profile picture and cover photo

--- spec/features/upload_profile_picture_spec.rb
+++ spec/features/upload_profile_picture_spec.rb
@@ -10,21 +10,32 @@ RSpec.feature 'UploadProfilePictures', type: :feature do
 
   context 'valid file' do
     scenario 'Upload profile picture' do
-      attach_file('user_avatar', Rails.root.join('spec/fixtures/files/sam.jpg'))
-      attach_file('user_cover_photo', Rails.root.join('spec/fixtures/files/sam.jpg'))
-      click_on 'Update profile pictures'
+      attach_file('user_avatar', Rails.root.join('spec/fixtures/files/sam.jpg'), make_visible: true)
 
-      expect(page).to have_css('img[src*="sam.jpg"]', count: 2)
+      expect(page).to have_css('img[src*="sam.jpg')
+    end
+
+    scenario 'Upload cover photo' do
+      banner = find('.banner')
+
+      expect do
+        attach_file('user_cover_photo', Rails.root.join('spec/fixtures/files/sam.jpg'), make_visible: true)
+      end.to(change do
+               banner[:style]
+             end)
     end
   end
 
   context 'invalid file' do
     scenario 'Upload other file type' do
-      attach_file('user_avatar', Rails.root.join('spec/fixtures/files/test.txt'))
-      attach_file('user_cover_photo', Rails.root.join('spec/fixtures/files/test.txt'))
-      click_on 'Update profile pictures'
+      attach_file('user_avatar', Rails.root.join('spec/fixtures/files/test.txt'), make_visible: true)
 
       expect(page).to have_content('Avatar has an invalid content type')
+    end
+
+    scenario 'Upload other file type on cover photo' do
+      attach_file('user_cover_photo', Rails.root.join('spec/fixtures/files/test.txt'), make_visible: true)
+
       expect(page).to have_content('Cover photo has an invalid content type')
     end
   end
