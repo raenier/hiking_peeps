9a72f5eb290fc3f944bc64414447fb08a7553cee
ren <raenierquiambao@gmail.com>
2024-09-14 14:29:39 +0800

    Make users confirm their accounts using their emails
    https://github.com/heartcombo/devise/wiki/How-To:-Add-:confirmable-to-Users
    basically add the confirmable tag on user and generate the migration needed
    then edit the existing users to allow them to login after this update

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password { Faker::Internet.password }
     password_confirmation { password }
+    confirmed_at { Time.now }
 
     profile { association :profile, user: instance }
 
