9a5410a279dd4a52e349b9c2d9d6d54f84801703
ren <raenierquiambao@gmail.com>
2024-09-25 15:56:26 +0800

    Show error when failed to login using github then redirect back to login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,8 +8,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
     else
-      session['devise.github_data'] = request.env['omniauth.auth'].except('extra')
-      redirect_to new_user_registration_url
+      set_flash_message(:notice, :failure, kind: 'Github', reason: @user.errors.full_messages.join(' '))
+      redirect_to new_user_session_url
+    end
+  end
 
   def google_oauth2
     @user = User.from_omniauth(request.env['omniauth.auth'])
