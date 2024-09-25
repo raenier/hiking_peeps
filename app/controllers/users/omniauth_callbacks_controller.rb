3959fddea9c451fff1fba6c8991b8843f75c92f3
ren <raenierquiambao@gmail.com>
2024-09-25 15:55:18 +0800

    Enable login using google oauth2
    Save image from profile

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,6 +10,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     else
       session['devise.github_data'] = request.env['omniauth.auth'].except('extra')
       redirect_to new_user_registration_url
+
+  def google_oauth2
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
+    else
+      set_flash_message(:notice, :failure, kind: 'Google', reason: @user.errors.full_messages.join(', '))
+      redirect_to new_user_session_url
     end
   end
 
